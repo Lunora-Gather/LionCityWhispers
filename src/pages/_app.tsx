9b92923d4846430dc41cbb170db3c6c77a6e222d
王洁湖 <25041425@hdu.edu.cn>
2026-06-26 10:54:25 +0800

    Import experience polish stylesheet

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import "@/styles/layout-polish.css";
+import "@/styles/experience-polish.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
