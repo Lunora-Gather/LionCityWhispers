76f58e6487222895ccb72eb6d7ff9bff2529cd50
王洁湖 <25041425@hdu.edu.cn>
2026-06-26 11:05:02 +0800

    Load experience polish layer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import "@/styles/polish.css";
+import "@/styles/experience-polish.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
